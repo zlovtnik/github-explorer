ef7dfc5a36a7f1dbe1f185779057e9e006e6e830
rcs <rafael.cardoso@synchro.com.br>
2022-03-09 23:03:30 -0300

    finalizando primeira parte

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,52 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const isDevelopment = process.env.NODE_ENV === "production";
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
     },
-    resolve: {
-        extensions: ['.js', '.jsx'],
+    port: 3004,
+    devMiddleware: {
+      publicPath: "https://localhost:3004",
     },
-    module: {
-        rules: [ 
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    }
+    hot: "only",
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
